c1b511efbc22c4b54b46766dbfbcce62d94045f8
Girish <girishramnani95@gmail.com>
2015-01-19 00:59:27 +0530

    next commit

---
+++ Jnext.py
@@ -0,0 +1,7 @@
+__author__ = 'Girish'
+
+import itertools
+def lexNext(num_li):
+    for z in range(len(num_li)-1,0,-1):
+
+
---
+++ common_div.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+__author__ = 'Girish'
+import math
+def gcd(a,b):
+    while(b):
+        a%=b
+        b^=a
+        a ^=b
+        b^=a
+    return a
+
+def no_div(n):
+    sqr = int(math.sqrt(n))
+    d = dict()
+    i=2
+    while n and i<=sqr:
+        if n%i==0:
+            d[i] = d.get(i,0)+1
+            n//=i
+        else:
+            i+=1
+    w=1
+    for x,y in d.items():
+        w*=(y+1)
+    return 2 if w==1 else w
+
+
+for x in range(int(input())):
+    li = [int(x) for x in input().split()]
+    print(no_div(gcd(*li)))
+

1fcc73b98620e4b29a5dd9616b77984ac0f5a937
Girish <girishramnani95@gmail.com>
2015-01-21 00:20:01 +0530

    Top level windows studied

---
+++ Tkinter_work/top_level.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+__author__ = 'Girish'
+from tkinter import *
+root = Tk()
+window= Toplevel(root)
+
+window.title("girish")
+window.lower()
+
+window.geometry("640x480+50+100" )
+root.mainloop()
+sleep(4)
+window.destroy()

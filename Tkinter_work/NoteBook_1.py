0ca5506796f25b58495676f09fd3fe878d4d4092
Girish <girishramnani95@gmail.com>
2015-01-21 09:47:43 +0530

    Notebook TTK

--- Tkinter_work/NoteBook_1.py
+++ Tkinter_work/NoteBook_1.py
@@ -1,1 +1,14 @@
 __author__ = 'Girish'
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+notebook=ttk.Notebook(root)
+
+frame1 = ttk.Frame(notebook,height=500,width=400)
+frame2 = ttk.Frame(notebook)
+notebook.add(frame1, text = 'One')
+notebook.add(frame2, text = 'Two')
+
+frame1.config(height=400,width=400)
+notebook.pack()
+root.mainloop()

ee72e7e3e66dae6391ae044f08dbd352893286b5
Girish <girishramnani95@gmail.com>
2015-01-28 01:23:36 +0530

    added more functionality

--- Qt/Qt_downloader/pydownloader.py
+++ Qt/Qt_downloader/pydownloader.py
@@ -5,6 +5,7 @@ from PyQt4.QtGui import QLineEdit
 from PyQt4.QtGui import QProgressBar
 from PyQt4.QtGui import QPushButton
 from PyQt4.QtGui import QVBoxLayout
+from PyQt4.QtGui import QFileDialog
 
 __author__ = 'Girish'
 import sys
@@ -21,15 +22,18 @@ class Py_downloader(QDialog):
         self.save_location = QLineEdit()
         self.progress = QProgressBar()
         button = QPushButton("Download")
+        browse = QPushButton("Browse")
 
         self.Url.setPlaceholderText("Enter the Url you wanna download")
         self.save_location.setPlaceholderText("Save file location")
+
 
         self.progress.setValue(0)
         self.progress.setAlignment(Qt.AlignHCenter)
         layout.addWidget(self.Url)
         layout.addWidget(self.save_location)
         layout.addWidget(self.progress)
+        layout.addWidget(browse)
         layout.addWidget(button)
 
         self.setLayout(layout)
@@ -37,15 +41,21 @@ class Py_downloader(QDialog):
         self.setWindowTitle("PyDownloader")
         self.setFocus()
         button.clicked.connect(self.download)
+        browse.clicked.connect(self.show_dialog)
+
+    def show_dialog(self):
+        self.save_file = QFileDialog.getSaveFileName(self,caption="Save File as ",directory=".",filter="all files (*.*)")
+        self.save_location.setText(self.save_file)
+
 
     def download(self):
         url = self.Url.text()
         save_location = self.save_location.text()
-        
+
         try:
             urllib.request.urlretrieve(url, save_location, self.report)
         except Exception:
-            QMessageBox.warning(self,"Warning","The downlaod failed")
+            QMessageBox.warning(self,"Warning","The download failed")
             self.reset()
             return
 

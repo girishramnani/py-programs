8bc6eda2d936f703e2bb73b173a1b9b90fdf4aca
Girish <girishramnani95@gmail.com>
2015-01-28 00:19:39 +0530

    next

--- Qt/Qt_downloader/pydownloader.py
+++ Qt/Qt_downloader/pydownloader.py
@@ -1,6 +1,9 @@
 from PyQt4.QtGui.QApplication import QApplication
 from PyQt4.QtGui.QDialog import QDialog
 from PyQt4.QtGui.QLineEdit import QLineEdit
+from PyQt4.QtGui.QProgressBar import QProgressBar
+from PyQt4.QtGui.QPushButton import QPushButton
+from PyQt4.QtGui.QVBoxLayout import QVBoxLayout
 
 __author__ = 'Girish'
 
@@ -9,10 +12,19 @@ class Py_downloader(QDialog):
     def __init__(self):
         super().__init__()
 
+    layout = QVBoxLayout()
+
+
 
     Url = QLineEdit()
     save_location  = QLineEdit()
+    progress = QProgressBar()
+    button  = QPushButton("Download")
 
+    layout.addWidget(Url)
+    layout.addWidget(save_location)
+    layout.addWidget(progress)
+    layout.addWidget(button)
 
 
 

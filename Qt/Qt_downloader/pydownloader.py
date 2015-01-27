6cf3c4120a423b3c1f032357f3c4c4be9aa449a8
Girish <girishramnani95@gmail.com>
2015-01-28 00:36:08 +0530

    created the report hook

--- Qt/Qt_downloader/pydownloader.py
+++ Qt/Qt_downloader/pydownloader.py
@@ -1,3 +1,4 @@
+from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QApplication
 from PyQt4.QtGui import QDialog
 from PyQt4.QtGui import QLineEdit
@@ -7,6 +8,8 @@ from PyQt4.QtGui import QVBoxLayout
 
 __author__ = 'Girish'
 import sys
+import urllib.request
+
 
 class Py_downloader(QDialog):
     def __init__(self):
@@ -16,26 +19,38 @@ class Py_downloader(QDialog):
 
 
 
-        Url = QLineEdit()
-        save_location  = QLineEdit()
-        progress = QProgressBar()
+        self.Url = QLineEdit()
+        self.save_location  = QLineEdit()
+        self.progress = QProgressBar()
         button  = QPushButton("Download")
 
-        Url.setPlaceholderText("Enter the Url you wanna download")
-        save_location.setPlaceholderText("Save file location")
+        self.Url.setPlaceholderText("Enter the Url you wanna download")
+        self.save_location.setPlaceholderText("Save file location")
 
-
-        layout.addWidget(Url)
-        layout.addWidget(save_location)
-        layout.addWidget(progress)
+        self.progress.setValue(0)
+        self.progress.setAlignment(Qt.AlignHCenter)
+        layout.addWidget(self.Url)
+        layout.addWidget(self.save_location)
+        layout.addWidget(self.progress)
         layout.addWidget(button)
 
         self.setLayout(layout)
 
         self.setWindowTitle("PyDownloader")
         self.setFocus()
+        button.clicked.connect(self.download)
 
+    def download(self):
+        url = self.Url.text()
+        save_location = self.save_location.text()
+        urllib.request.urlretrieve(url,save_location,self.report)
 
+    def report(self,blocknum,blocksize,totalsize):
+        readsofar = blocknum*blocksize
+        if totalsize >0:
+            percent = readsofar *100 / totalsize
+            self.progress.setValue(int(percent))
+            
 
 
 app = QApplication(sys.argv)

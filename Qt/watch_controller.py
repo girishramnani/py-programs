006568a51393f44a23cb1266f3925fc77ed9f19f
girishramnani <girishramnani95@gmail.com>
2015-02-03 03:56:08 +0530

    lcd watch

--- Qt/watch_controller.py
+++ Qt/watch_controller.py
@@ -5,20 +5,29 @@ import sys
 from Qt.watch import Ui_Dialog
 import datetime
 import time
+from threading import Timer
 
 class working(QDialog,Ui_Dialog):
-    def __init__(self):
 
+    def __init__(self):
         super().__init__()
         self.setWindowTitle("Time")
         self.setupUi(self)
-        self.functioning()
+        self.t = QTimer()
+        self.t.timeout.connect(self.functioning)
+        self.t.start(1000)
+
     def functioning(self):
-
         t = time.localtime()
-        w = time.strftime("%H %M",t)
+        h = time.strftime("%H",t)
+        m = time.strftime("%M",t)
+        s = time.strftime("%S",t)
+        self.hour.setProperty("value",h)
+        self.second.setProperty("value",s)
+        self.minute.setProperty("value",m)
 
-        
+
+
 
 
 

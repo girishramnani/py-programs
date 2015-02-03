6dd283e34cc9f731dd0b99c1bc683a01d8fa59b8
girishramnani <girishramnani95@gmail.com>
2015-02-03 22:21:26 +0530

    systray

--- Qt/sign_in_impl.py
+++ Qt/sign_in_impl.py
@@ -1,4 +1,4 @@
-from PyQt4.QtGui import QDialog, QApplication, QPixmap
+from PyQt4.QtGui import QDialog, QApplication, QPixmap, QIcon, QSystemTrayIcon, QMenu, QAction
 from PyQt4.QtGui import QSplashScreen
 
 __author__ = 'Girish'
@@ -10,10 +10,25 @@ class View(QDialog,signin.Ui_Dialog):
     def __init__(self):
         QDialog.__init__(self)
         self.setupUi(self)
+        systry = QIcon("jBli3.png")
+        systry_icon = QSystemTrayIcon(systry,self)
+        menu =QMenu()
+        restore  = QAction("Restore",self)
+
+        close = QAction("Close",self)
+        menu.addAction(restore)
+        menu.addAction(close)
+        systry_icon.setContextMenu(menu)
+
+        systry_icon.show()
+        systry_icon.showMessage("heyo","Hello")
+        close.triggered.connect(self.close)
+
 import sys
 app = QApplication(sys.argv)
 dialog = View()
 dialog.show()
+
 '''for splash'''
 # splash_image = QPixmap()
 # splash =QSplashScreen(splash_image)

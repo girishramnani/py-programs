ac8bf2ecb38e6e7b6bda9ce68dd81967be00f9ae
girishramnani <girishramnani95@gmail.com>
2015-02-04 15:41:21 +0530

    system tray for downloader

--- Qt/system_tray_impl.py
+++ Qt/system_tray_impl.py
@@ -1,3 +1,5 @@
+from PyQt4.QtCore import QTimer
+
 __author__ = 'Girish'
 import shelve
 from PyQt4.QtGui import QIcon, QSystemTrayIcon, QApplication
@@ -11,13 +13,19 @@ import sys
 
 class Message_display:
     def __init__(self):
-        t =QApplication(sys.argv)
+        self.t =QApplication(sys.argv)
         icon = QIcon("jBli3.png")
         self.tray = QSystemTrayIcon(icon)
-        self.tray.show()
-        t.exec_()
-    def show_message(self,download):
-        self.tray.showMessage("Downloading",download)
 
-x =Message_display()
+        self.tray.show()
+        self.tray.showMessage("Downloading","sdf")
+
+    def show_message(self,download):
+        def show():
+            self.tray.showMessage("Downloading",download)
+        return show
+    def exec_(self):
+        self.t.exec_()
+
+
 
